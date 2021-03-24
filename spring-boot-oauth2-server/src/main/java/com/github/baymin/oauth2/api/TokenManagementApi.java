8b70bdd8305df05fcec09ff3225d675da2bd8d12
zongweibai <jonezw@163.com>
2021-03-24 16:07:19 +0800

    feat: add oauth2 token query endpoint

--- spring-boot-oauth2-server/src/main/java/com/github/baymin/oauth2/api/TokenManagementApi.java
+++ spring-boot-oauth2-server/src/main/java/com/github/baymin/oauth2/api/TokenManagementApi.java
@@ -9,11 +9,9 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.Objects;
 
 /**
@@ -48,6 +46,21 @@ public class TokenManagementApi {
             tokenServices.revokeToken(token);
         } else if (StringUtils.equalsIgnoreCase(tokenType, OAuth2Constant.TOKEN_TYPE_REFRESH)) {
             ((RedisTokenStore) tokenStore).removeRefreshToken(token);
+        }
+    }
+
+    /**
+     * 根据clientId查询所有的Token
+     *
+     * @return list
+     */
+    @GetMapping(value = "/oauth/token/{clientId}")
+    public Collection<OAuth2AccessToken> fetchTokenByClientId(@PathVariable("clientId") String clientId,
+                                                              @RequestParam(value = "userName", required = false) String userName) {
+        if (StringUtils.isBlank(userName)) {
+            return tokenStore.findTokensByClientId(clientId);
+        } else {
+            return tokenStore.findTokensByClientIdAndUserName(clientId, userName);
         }
     }
 
